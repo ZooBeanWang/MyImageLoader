758e2ef5ff00142d29438a2a75797e9f30b6a7e2
zoobeanwang <zoobeanwang@gmail.com>
2016-07-16 16:42:49 +0800

    create project & create ImageLoader version 1.0
    Signed-off-by: zoobeanwang <zoobeanwang@gmail.com>

---
+++ src/main/java/com/pattern/test/imageloader/ImageLodaer.java
@@ -0,0 +1,69 @@
+package com.pattern.test.imageloader;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.LruCache;
+import android.widget.ImageView;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by frog on 7/16/16.
+ */
+public class ImageLodaer {
+    //图片缓存
+    LruCache<String, Bitmap> mImageCache;
+    //线程池，线程数量为CPU数量
+    ExecutorService mExecutorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    public ImageLodaer() {
+        initImageCache();
+    }
+
+    private void initImageCache() {
+        final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+        final int cacheSize = maxMemory / 4;
+        mImageCache = new LruCache<String, Bitmap>(cacheSize) {
+            @Override
+            protected int sizeOf(String key, Bitmap value) {
+                return value.getRowBytes() * value.getHeight() / 1024;
+            }
+        };
+    }
+
+    public void displayImage(final String url, final ImageView imageView) {
+        imageView.setTag(url);
+        mExecutorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                Bitmap bitmap = downloadImage(url);
+                if (bitmap == null) {
+                    return;
+                }
+                if (imageView.getTag().equals(url)) {
+                    imageView.setImageBitmap(bitmap);
+                }
+                mImageCache.put(url, bitmap);
+            }
+        });
+
+    }
+
+    private Bitmap downloadImage(String imageUrl) {
+        Bitmap bitmap = null;
+        try {
+            URL url = new URL(imageUrl);
+            final HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            bitmap = BitmapFactory.decodeStream(conn.getInputStream());
+            conn.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+
+    }
+
+}
